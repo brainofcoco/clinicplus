dceb3ad0d5c87521fe5e36f1b1677b1a3a28dc7b
Thaywo <48289615+thaywo@users.noreply.github.com>
2021-09-22 17:13:31 +0100

    Added the forgot Password Modal

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -63,6 +63,30 @@
             </div>
         </div>
     </div>
+
+    <div aria-hidden="true" aria-labelledby="myModalLabel" role="dialog" tabindex="-1" id="myModal" class="modal fade">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <form method="post" action="auth/forgot_password">
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                        <h4 class="modal-title">Forgot Password ?</h4>
+                    </div>
+
+                    <div class="modal-body">
+                        <p>Enter your e-mail address below to reset your password.</p>
+                        <input type="text" name="email" placeholder="Email" autocomplete="off" class="form-control placeholder-no-fix">
+
+                    </div>
+                    <div class="modal-footer">
+                        <button data-dismiss="modal" class="btn btn-default" type="button">Cancel</button>
+                        <input class="btn btn-success" type="submit" name="submit" value="submit">
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
 <script src="common/core-assets/login/js/jquery.min.js"></script>
 <script src="common/core-assets/login/js/popper.min.js"></script>
 <script src="common/core-assets/login/js/bootstrap.min.js"></script>
